77b3157433354f5bddfeef8f1bcae365c293f4cc
vishalranjan14@gmail.com <vishalranjan14@gmail.com>
2023-12-23 12:19:13 +0530

    c

---
+++ numbers.js
@@ -0,0 +1,10 @@
+//lets deep dive into Numbers theory in Javascript 
+
+
+const score =300
+console.log(score)
+console.log(typeof(score))
+
+
+const num = new Number(100)
+console.log(num )
---
+++ string.js
@@ -0,0 +1,32 @@
+var a = "Hey Man how are you doing "
+
+console.log(a)
+
+var name = 'Vishal Ranjan'
+
+var name = 'CoderGame'
+
+//another way to declare a string 
+var p = new String('pig')
+
+// console.log(p)
+// var count = 23
+
+// //string interpolation
+// console.log(`${name} and my repo count is ${count}`)
+// //to Upper
+// console.log(name.toUpperCase());
+
+// console.log(name);
+// //char at index
+// console.log(name.charAt(2));
+
+// console.log(name.indexOf('x'))
+
+// console.log(name.substring(0, 3))
+
+const newstring= name.slice(-4,0)
+console.log(newstring)
+//another way to declare a string 
+
+ 
